7a1c41b6d42c03a1fa0dc09e1ae5ee842a8c0dee
Mona Lisa <uvshz@student.kit.edu>
2017-08-15 14:40:37 +0200

    Javadoc + final
    test if i could commit useless stuff here

--- scripts/hello_world.java
+++ scripts/hello_world.java
@@ -1,10 +1,14 @@
 /**
  * Hello World program
  */
-public class hello_world {
+public final class hello_world {
 
 	/**
 	 * Main method
+	 * <p>
+	 * the method parameter will be ignored in this case
+	 *
+	 * @param args String[] String-Array with the command Line Input
 	 */
 	public static void main(String[] args) {
 		// A wild comment
